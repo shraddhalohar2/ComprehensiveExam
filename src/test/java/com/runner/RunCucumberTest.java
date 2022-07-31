d6cb0173aa9f5cafebdac4c3eccfc5ff915a7b23
shraddhalohar2 <Shraddha.Lohar@mindtree.com>
2022-07-31 18:27:54 +0530

    bdd

---
+++ src/test/java/com/runner/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package com.runner;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="features",glue="com.stepdefinition")
+
+public class RunCucumberTest {
+
+}
